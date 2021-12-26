498cf58a5ad0505da5a7436e0a222462296d4763
sispoof <mbenhamouda099@gmail.com>
2021-12-26 16:21:27 +0100

    Add CustomTileChat(ListTile) widget to a ListView

---
+++ lib/components/CustomTileChat.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class CustomTileChat extends StatelessWidget {
+  const CustomTileChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
+      ),
+      trailing: Text('18:04'),
+      title: Text(
+        'Ahmed BH',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(Icons.done_all),
+          SizedBox(
+            width: 4,
+          ),
+          Text(
+            "Hi ahmed!",
+            style: TextStyle(fontSize: 13),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/ChatPage.dart
+++ lib/pages/ChatPage.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:front/components/CustomTileChat.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -16,6 +17,13 @@ class _ChatPageState extends State<ChatPage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () => print('floatingButton'),
         child: Icon(Icons.chat),
+      ),
+      body: ListView(
+        children: [
+          CustomTileChat(),
+          CustomTileChat(),
+          CustomTileChat(),
+        ],
       ),
     );
   }
