1e7e348769e5b2f2396084ed4ee2fb2c211604a2
sispoof <mbenhamouda099@gmail.com>
2021-12-27 15:07:36 +0100

    add chat List (instance of Chat Model) and replace ListView to ListView.builder to make it dynamic

--- lib/pages/ChatPage.dart
+++ lib/pages/ChatPage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:front/Models/ChatModel.dart';
 import 'package:front/components/CustomTileChat.dart';
 
 class ChatPage extends StatefulWidget {
@@ -11,6 +12,44 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatModel> chats = [
+    ChatModel(
+        name: "Ahmed Ben Hamouda",
+        isGroup: false,
+        currentMessage: "Hi How are you!",
+        time: "4:00",
+        icon: "person.svg"),
+    ChatModel(
+        name: "Flech Family",
+        isGroup: true,
+        currentMessage: "famma?",
+        time: "10:00",
+        icon: "groups.svg"),
+    ChatModel(
+        name: "Mourad krakeb",
+        isGroup: false,
+        currentMessage: "hello!",
+        time: "13:00",
+        icon: "person.svg"),
+    ChatModel(
+        name: "karim jounta",
+        isGroup: false,
+        currentMessage: "ana fel sale de jeux a5lat",
+        time: "00:00",
+        icon: "person.svg"),
+    ChatModel(
+        name: "DSI 32",
+        isGroup: true,
+        currentMessage: "ghodwa mana9rawech",
+        time: "21:00",
+        icon: "groups.svg"),
+    ChatModel(
+        name: "Ahmed Ben Hamouda",
+        isGroup: false,
+        currentMessage: "Happy birthday!",
+        time: "00:01",
+        icon: "person.svg"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +57,10 @@ class _ChatPageState extends State<ChatPage> {
         onPressed: () => print('floatingButton'),
         child: Icon(Icons.chat),
       ),
-      body: ListView(
-        children: [
-          CustomTileChat(),
-          CustomTileChat(),
-          CustomTileChat(),
-        ],
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index) =>
+            CustomTileChat(chatModel: chats[index]),
       ),
     );
   }
