49fed536f938db3b44fecee6e7baa41762c92644
sispoof <mbenhamouda099@gmail.com>
2021-12-28 20:40:40 +0100

    remove inkWell and handle click event outside

--- lib/components/ContactTile.dart
+++ lib/components/ContactTile.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, file_names, prefer_const_constructors
+// ignore_for_file: unused_import, file_names, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,31 +10,51 @@ class ContactTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => print('stop tapping'),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 23,
-          child: SvgPicture.asset(
-            "assets/svg/person.svg",
-            color: Colors.white,
-            height: 30,
-            width: 30,
-          ),
-          backgroundColor: Colors.blueGrey[200],
+    return ListTile(
+      leading: Container(
+        height: 53,
+        width: 50,
+        child: Stack(
+          children: [
+            CircleAvatar(
+              radius: 23,
+              child: SvgPicture.asset(
+                "assets/svg/person.svg",
+                color: Colors.white,
+                height: 30,
+                width: 30,
+              ),
+              backgroundColor: Colors.blueGrey[200],
+            ),
+            contacts.selected
+                ? Positioned(
+                    bottom: 4,
+                    right: 5,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.teal,
+                      radius: 11,
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                    ),
+                  )
+                : Container(),
+          ],
         ),
-        title: Text(
-          contacts.name.toString(),
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
+      ),
+      title: Text(
+        contacts.name.toString(),
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
         ),
-        subtitle: Text(
-          contacts.status.toString(),
-          style: TextStyle(
-            fontSize: 13,
-          ),
+      ),
+      subtitle: Text(
+        contacts.status.toString(),
+        style: TextStyle(
+          fontSize: 13,
         ),
       ),
     );
