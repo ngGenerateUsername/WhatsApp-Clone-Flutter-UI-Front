147b57dfd1615b43d1853aca626056e881cf9b07
sispoof <mbenhamouda099@gmail.com>
2021-12-28 15:44:19 +0100

    add new Widget screen for contact list

---
+++ lib/Screens/SelectContact.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: file_names, unused_import, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+
+class SelectContact extends StatefulWidget {
+  const SelectContact({Key? key}) : super(key: key);
+
+  @override
+  _SelectContactState createState() => _SelectContactState();
+}
+
+class _SelectContactState extends State<SelectContact> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Select Contact",
+              style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "25 contacts",
+              style: TextStyle(
+                fontSize: 13,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              size: 26,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
