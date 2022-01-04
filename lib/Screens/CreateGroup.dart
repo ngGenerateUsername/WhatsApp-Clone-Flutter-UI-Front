eaa247ba65d82cfd8502b1f3fb7a16f51be9e86e
sispoof <mbenhamouda099@gmail.com>
2022-01-04 17:02:56 +0100

    complete create group screen

--- lib/Screens/CreateGroup.dart
+++ lib/Screens/CreateGroup.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: file_names, unused_import, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, unused_field, curly_braces_in_flow_control_structures
+// ignore_for_file: file_names, unused_import, prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, unused_field, curly_braces_in_flow_control_structures, prefer_is_empty
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:front/Models/ChatModel.dart';
 import 'package:front/components/ContactTile.dart';
 import 'package:front/main.dart';
@@ -96,49 +97,108 @@ class _CreateGroupState extends State<CreateGroup> {
             ),
           ],
         ),
-        body: ListView.builder(
-          itemCount: _contacts.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () {
-                if (_contacts[index].selected == false) {
-                  setState(() {
-                    _contacts[index].selected = true;
-                    groups.add(_contacts[index]);
-                  });
-                } else {
-                  setState(() {
-                    _contacts[index].selected = false;
-                    groups.remove(_contacts[index]);
-                  });
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemCount: _contacts.length + 1,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return Container(
+                    height: groups.length > 0 ? 90 : 10,
+                  );
                 }
+                return InkWell(
+                  onTap: () {
+                    if (_contacts[index - 1].selected == false) {
+                      setState(() {
+                        _contacts[index - 1].selected = true;
+                        groups.add(_contacts[index - 1]);
+                      });
+                    } else {
+                      setState(() {
+                        _contacts[index - 1].selected = false;
+                        groups.remove(_contacts[index - 1]);
+                      });
+                    }
+                  },
+                  child: ContactTile(contacts: _contacts[index - 1]),
+                );
               },
-              child: ContactTile(contacts: _contacts[index]),
-            );
-          },
+            ),
+            groups.length > 0
+                ? Column(
+                    children: [
+                      Container(
+                        height: 75,
+                        color: Colors.white,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: groups.length,
+                          itemBuilder: (context, index) => InkWell(
+                            onTap: () {
+                              setState(() {
+                                int finder = _contacts.indexOf(groups[index]);
+                                _contacts[finder].selected = false;
+                                groups.remove(groups[index]);
+                              });
+                            },
+                            child: AvatarCard(groups[index].name.toString()),
+                          ),
+                        ),
+                      ),
+                      Divider(
+                        thickness: 1,
+                      ),
+                    ],
+                  )
+                : Container(),
+          ],
         ));
   }
 
-  Widget primaryCard(String name, IconData icon) {
-    return InkWell(
-      onTap: () => print("inkWell"),
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 23,
-          child: Icon(
-            icon,
-            size: 26,
-            color: Colors.white,
+  Widget AvatarCard(String name) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Stack(
+            children: [
+              CircleAvatar(
+                radius: 23,
+                child: SvgPicture.asset(
+                  "assets/svg/person.svg",
+                  color: Colors.white,
+                  height: 30,
+                  width: 30,
+                ),
+                backgroundColor: Colors.blueGrey[200],
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 11,
+                  child: Icon(
+                    Icons.clear,
+                    color: Colors.white,
+                    size: 13,
+                  ),
+                ),
+              ),
+            ],
           ),
-          backgroundColor: Color(0xFF25D366),
-        ),
-        title: Text(
-          name,
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
+          SizedBox(
+            height: 2,
           ),
-        ),
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 13,
+            ),
+          ),
+        ],
       ),
     );
   }
